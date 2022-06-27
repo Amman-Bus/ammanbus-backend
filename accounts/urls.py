95c5779b989d04a2ba359c7bd581e711c89a6f69
BatoolBtoush <batoolbtoush98@gmail.com>
2022-06-27 22:25:33 +0300

    accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
+    path("accounts/auth", include("django.contrib.auth.urls")),
     path('register/', views.register, name='register'),
     path('login/', views.login_user, name='login_user'),
     path('logout/', views.logout_user, name='logout_user'),
     path('home/', views.home, name='home'),
-    path('passenger_register/', views.passenger_register.as_view(), name='passenger_register'),
-    path('driver_register/', views.driver_register.as_view(), name='driver_register')
+    path('passenger_register/', views.passenger_register, name='passenger_register'),
+    path('driver_register/', views.driver_register.as_view(), name='driver_register'),
+    path('driverdash/', views.driverdash, name='driverdash'),
+    path('passengerdash/', views.passengerdash, name='passengerdash')
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,8 @@ from django.contrib import messages
 from django.views.generic import CreateView
 from .models import User
 from .forms import PassengerSignUpForm, DriverSignUpForm
+from django.urls import reverse
+
 
 # Create your views here.
 User = get_user_model()
@@ -16,15 +18,32 @@ def home(request):
 def register(request):
     return render(request, 'register.html')
 
-class passenger_register(CreateView):
-    model = User  
-    form_class = PassengerSignUpForm
-    template_name= 'passenger_register.html'
+def driverdash(request):
+    return render(request, 'driverdash.html')
 
-    def form_valid(self, form):
-        user = form.save()
-        login(self.request, user)
-        return redirect('/accounts/home')
+def passengerdash(request):
+    return render(request, 'passengerdash.html')
+
+def passenger_register(request):
+    # model = User  
+    # form_class = PassengerSignUpForm
+    # template_name= 'passenger_register.html'
+
+    # def form_valid(self, form):
+    #     user = form.save()
+    #     login(self.request, user)
+    #     return redirect('/accounts/home')
+    if request.method == "GET":
+        return render(
+            request, "passenger_register.html",
+            {"form": PassengerSignUpForm}
+        )
+    elif request.method == "POST":
+        form = PassengerSignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse("passengerdash"))
 
 class driver_register(CreateView):
     model = User  
@@ -34,7 +53,7 @@ class driver_register(CreateView):
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('/accounts/home')
+        return redirect('/accounts/driverdash')
 
 def login_user(request):
     if request.method=='POST':
@@ -43,6 +62,12 @@ def login_user(request):
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
+            # if isinstance(user,driver_register ):
+            #     login(request,user)
+            #     return redirect('/accounts/driverdash')
+            # elif isinstance(user,passenger_register):
+            #     login(request,user)
+            #     return redirect('/accounts/passengerdash')
             if user is not None :
                 login(request,user)
                 return redirect('/accounts/home')
